7a334b0d2c594755f7afd800c41861b994aa073e
seojinvely1004 <seojin0124@naver.com>
2022-12-05 11:39:25 +0900

    constructor(), render()

--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ import './index.css';
 
 import reportWebVitals from './reportWebVitals';
 
-import ConstructorTest from './ConstructorTest';
+{/*import ConstructorTest from './ConstructorTest';*/}
+import Book from './Book';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 
-    *<ConstructorTest writer="작성자"/>*
+    {/*<ConstructorTest writer="작성자"/>*/}
+    <Book title="리액트" body="리액트이야기를 재미나게 풀어봅니다" price="30000"/> 
+
+    <Book title="스프링" body="mvc는 물론 부트까지도 다룹니다" price="40000"/> 
+    <Book title="마리아db" body="오라클 mysql mariadb비교하여 설명합니다" price="25000"/> 
     
 
   </React.StrictMode>
