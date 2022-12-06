f0412158927d49b3d31c1bbad1bdfb071551f85a
seojinvely1004 <seojin0124@naver.com>
2022-12-06 15:31:52 +0900

    feat(React) : boardList "delete"구현완료

--- src/BoardList.js
+++ src/BoardList.js
@@ -11,10 +11,16 @@ class BoardHeading extends React.Component{
     }
 class Board extends React.Component{
     render() {               
+        const deleteHandler = (e) =>{
+            //삭제버튼 클릭한 게시물을 게시물 리스트에서 삭제 - 화면 렌더링
+            this.props.deleteBoard(e.target.id); //deleteBoard있으면 삭제할id를주겠다.
+            //배열에서 몇번째 게시물을 삭제하는지 
+        }
+
         const board_tr = this.props.body.map(function(oneboard, index) {
             return (<tr key={index}><td>{oneboard.seq}</td><td>{oneboard.title}</td>
             <td>{oneboard.writer}</td><td>{++oneboard.viewcount}</td>
-            <td><button>삭제</button></td><td><button>수정</button></td></tr>);
+            <td><button id={index} onClick={deleteHandler}>삭제</button></td><td><button>수정</button></td></tr>);
         });    
 
         return(
@@ -22,16 +28,34 @@ class Board extends React.Component{
         );
     }
 }
-class BoardInsertForm extends React.Component{
-    render()//제목 :입력창
-    //작성자 : 입력창
-    // 글추가 버튼
 
+    //제목: 입력창, 작성자 : 입력창// 글추가 버튼
     //제목입력값, 작성자입력값을 전달받아서 글추가버튼 클릭시
     //번호 : 게시물리스트 갯수 + 1로 구현
     //조회수 : 0 (글작성하는것이니까)
     //게시물 1개 객체 생성
     //props.addBoard(게시물 1개 객체 전달)
+class BoardInsertForm extends React.Component{
+    state = {title : '', writer : '', seq : this.props.size +1, viewcount : 0};
+  
+    render(){
+        const changeHandler = (e)=>{
+            this.setState({ [e.target.id] : e.target.value});
+        }
+        const insertHandler = ()=>{
+            this.setState({seq : this.state.seq +1})
+            this.props.addBoard({seq: this.state.seq, title: this.state.title,
+            writer:this.state.writer, viewcount :this.state.viewcount });
+        }
+
+        return(
+            <div>
+            제목입력:<input type="text" id="title" onChange={changeHandler}/>
+            작성자입력:<input type="text" id="writer" onChange={changeHandler}/>
+            <button onClick = {insertHandler}>글추가버튼</button>
+            </div>
+        );    
+    }
 }
 
 class BoardList extends React.Component{
@@ -47,20 +71,32 @@ class BoardList extends React.Component{
     }//constructor
 
     render(){
-
          //addBoard 구현
          //1. 입력된 게시물 1개 전달
          //2. 1번 전달 게시물을 게시물리스트 추가 
          //3. state 게시물 리스트값 변경
 
+        const addBoard = (newBoard)=>{//1.
+            this.state.boardList.push(newBoard);//2.
+            this.setState({boardList : this.state.boardList});//3.
+        }
+
+        //Board의 this.props.deleteBoard(e.target.id); 
+        const deleteBoard = (index)=>{
+            //추가 : push, 반복 : map, 삭제 : filter, splice,,
+            this.state.boardList.splice(index, 1);//배열갯수변화
+            this.setState({boardList : this.state.boardList});//렌더링되도록 변경된 값 알려주기
+
+        }
+
     return (
         <div>
             <table border="3">
                 <BoardHeading />
-                <Board body = {this.boardList} />
-                <br/>
-                <BoardInsertForm addBoard={addBoard}/>
+                <Board body = {this.state.boardList} deleteBoard = {deleteBoard}/>
             </table>
+            <br/><br/>
+            <BoardInsertForm size = {this.state.boardList.length}addBoard={addBoard}/>
         </div>
     );
     }
